358fa0d479f0246d501c8b76e051ed35d2782edd
tony <vemupawankumar2.0@gmail.com>
2023-05-14 07:10:21 +0530

    first commite

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: { origin: "*", methods: ["GET", "POST"] },
 });
-// const path = require("path");
 
+const port = "https://videochat-qesc.onrender.com" || 8000;
+server.listen(port, () => console.log(`server is running on port ${port}`));
 const rooms = {};
 
 io.on("connection", (socket) => {
@@ -42,13 +43,3 @@ io.on("connection", (socket) => {
     io.to(incoming.target).emit("ice-candidate", incoming.candidate);
   });
 });
-
-// if (process.env.PROD) {
-//   app.use(express.static(path.join(__dirname, "./client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//   });
-// }
-
-const port = process.env.PORT || 8000;
-server.listen(port, () => console.log(`server is running on port ${port}`));
