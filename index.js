0e61f2400e7ecdb6196c726a4ae42dd9b358b8f1
tony <vemupawankumar2.0@gmail.com>
2023-05-23 06:45:17 +0530

    commited

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://videochat-qesc.onrender.com",
+    origin: "https://videochat-qesc.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
